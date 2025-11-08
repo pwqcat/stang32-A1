53d0c7100a255e1fb179a623ea3ffbdc0afbaced
pwqcat <https://github.com/pwqcat/stang32-A1>
2025-11-08 10:58:40 +0800

    feat: Add AssignmentOne entry point

---
+++ src/AssignmentOne.java
@@ -0,0 +1,6 @@
+public class AssignmentOne {
+    public static void main(String[] args) {
+        System.out.println("Assignment 1 Started!");
+        System.out.println("Project for user: stang32");
+    }
+}
