5cf71ca6068e9e00be4a8f4904620b17cfecdcaf
pwqcat <https://github.com/pwqcat/stang32-A1>
2025-11-08 10:46:36 +0800

    feat: Initial project setup with HealthProfessional class

---
+++ src/HealthProfessional.java
@@ -0,0 +1,25 @@
+public class HealthProfessional {
+    protected int id;
+    protected String name;
+    protected String specialty;
+
+    /**
+     * Default constructor.
+     * Creates a HealthProfessional object with default empty values.
+     */
+    public HealthProfessional() {
+    }
+
+    /**
+     * Parameterized constructor.
+     * Creates a HealthProfessional object with specified initial values.
+     * @param id The professional's ID.
+     * @param name The professional's name.
+     * @param specialty The professional's specialty.
+     */
+    public HealthProfessional(int id, String name, String specialty) {
+        this.id = id;
+        this.name = name;
+        this.specialty = specialty;
+    }
+}
